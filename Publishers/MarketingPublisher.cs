18ca1263f3254ea690d14fdf1671e548c40c0e3f
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-15 15:37:46 +0300

    SmartHub Notification System - Marketing Publisher class (refactored)

--- Publishers/MarketingPublisher.cs
+++ Publishers/MarketingPublisher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartHub_NotificatioSystem.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,44 @@ using System.Threading.Tasks;
 
 namespace SmartHub_NotificatioSystem.Publishers
 {
-    internal class MarketingPublisher
+    public class MarketingPublisher : Publisher
     {
+        public MarketingPublisher(string id, string name, string description) : base(id, name, description)
+        {
+
+        }
+
+        //Needs one event - Marketing Campaign Launched
+        public event EventHandler<NotificationEventArgs> MarketingCampaignLaunched;
+
+        //Launch Campaign Required
+        public void LaunchCampaign(int notificationId, string notificationType, string notificationDescription, string notificatinoMessage)
+        {
+            if (notificationId == 0)
+            {
+                Console.WriteLine("Invalid Notificatin id.Try again!");
+                return;
+            }
+
+            Console.WriteLine($"Launching market campaign: {notificatinoMessage} with Descrption as follows {notificationDescription}");
+
+            //to bundle all the notification details in one package
+            var args = new NotificationEventArgs(
+                       notificationId,
+                       notificationType,
+                       notificationDescription,
+                       notificatinoMessage);
+
+            onMarketingCampaignLaunched(args);
+
+        }
+
+        //Protected method
+        protected virtual void onMarketingCampaignLaunched(NotificationEventArgs e) 
+        {
+            //Trigger the MarketingCampaignLaunch
+            MarketingCampaignLaunched?.Invoke(this, e);
+        }
+
     }
 }
