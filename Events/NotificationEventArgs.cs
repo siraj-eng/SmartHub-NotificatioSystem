4428e70021b2f104af1839174d9a5dc3aa1bae1a
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-15 09:36:35 +0300

    SmartHub Notification System - Custom Delegate data type

---
+++ Events/NotificationEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHub_NotificatioSystem.Events
+{
+    public class NotificationEventArgs : EventArgs
+    {
+        public int NotificationId { get; set; }
+        public string NotificationMessage { get; set; }
+        public string NotificationType { get; set; }
+        public string NotificationDescription { get; set; }
+        public NotificationEventArgs (string notification) => NotificationMessage = notification;
+
+        //Constructor Overloading
+        public NotificationEventArgs(int notificationId, string notificationType, string notificationDescription, string notificationMessage)
+        {
+            NotificationId = notificationId;
+            NotificationType = notificationType;
+            NotificationDescription = notificationDescription;
+            NotificationMessage = notificationMessage;
+        }
+    }
+}
