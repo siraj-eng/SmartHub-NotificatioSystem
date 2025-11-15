4066b822d98b2380ce9667f4614e85a3de4ff961
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-15 10:48:03 +0300

    SmartHub Notification System - Notification class

--- Models/Notification.cs
+++ Models/Notification.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace SmartHub_NotificatioSystem.Models
 {
-    internal class NotificationHub
+    public class Notification
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+        //Constructor
+        public Notification(int id, string name, string description)
+        {
+            Id = id;    
+            Name = name;
+            Description = description;
+
+        }
+
     }
 }
