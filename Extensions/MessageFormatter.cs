30c93fb5b79d517fd35cc39b02e3f9e030be71c7
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-16 15:14:29 +0300

    SmartHub Notification System - Utilities.cs

--- Extensions/MessageFormatter.cs
+++ Extensions/MessageFormatter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartHub_NotificatioSystem.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace SmartHub_NotificatioSystem.Extensions
 {
-    internal class MessageFormatter
+    public static class MessageFormatter
     {
+        public static string FormatMessage(this NotificationEventArgs notification)
+        {
+            return $"[Notification] Type: {notification.NotificationType}, Message: {notification.NotificationMessage}";
+        }
     }
 }
