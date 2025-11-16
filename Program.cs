6ed7ad4412a8f3ae940a7d8c7b67cc11d0835b19
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-16 15:31:47 +0300

    SmartHub Notification System - Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SmartHub_NotificatioSystem.Hub;
+using SmartHub_NotificatioSystem.Publishers;
+using SmartHub_NotificatioSystem.Subscribers;
+using SmartHub_NotificatioSystem.Events;
+using SmartHub_NotificatioSystem.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartHub_NotificatioSystem
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hub = new NotificationHub();
+            var marketingPublisher = new MarketingPublisher("M001", "MarketingPublisher", "Publishes marketing campaigns");
+            marketingPublisher.MarketingCampaignLaunched += async (sender, e) => await hub.PublishNotification(e);
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("\n=== Smart Notifications Hub ===");
+                Console.WriteLine("1. Add Subscriber");
+                Console.WriteLine("2. Remove Subscriber");
+                Console.WriteLine("3. List Subscribers");
+                Console.WriteLine("4. Publish System Notification");
+                Console.WriteLine("5. Launch Marketing Campaign");
+                Console.WriteLine("6. Exit");
+                Console.Write("Choose an option: ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Enter Name: ");
+                        var name = Console.ReadLine();
+                        Console.Write("Enter Description: ");
+                        var desc = Console.ReadLine();
+                        Console.Write("Enter Details: ");
+                        var details = Console.ReadLine();
+                        Console.Write("Enter Subscription Types (comma separated, e.g., System,Marketing): ");
+                        var types = Console.ReadLine()?.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                        var subscriber = new Subscriber(GenerateId(), name, desc, details, new List<string>(types ?? Array.Empty<string>()));
+                        hub.Subscribe(subscriber);
+                        break;
+
+                    case "2":
+                        Console.Write("Enter Subscriber ID to remove: ");
+                        if (int.TryParse(Console.ReadLine(), out int removeId))
+                        {
+                            var toRemove = hub.Subscribers.FirstOrDefault(s => s.Id == removeId);
+                            if (toRemove != null)
+                                hub.Unsubscribe(toRemove);
+                        }
+                        break;
+
+                    case "3":
+                        Console.WriteLine("=== Subscribers ===");
+                        foreach (var sub in hub.Subscribers)
+                        {
+                            Console.WriteLine($"{sub.Id}: {sub.Name} - {string.Join(",", sub.SubscriptionPreferences)}");
+                        }
+                        break;
+
+                    case "4":
+                        Console.Write("Enter Notification Message: ");
+                        var msg = Console.ReadLine();
+                        var systemNotification = new NotificationEventArgs(GenerateId(), Constants.SystemType, "System Update", msg);
+                        await hub.PublishNotification(systemNotification);
+                        break;
+
+                    case "5":
+                        Console.Write("Enter Marketing Campaign Message: ");
+                        var marketingMsg = Console.ReadLine();
+                        marketingPublisher.LaunchCampaign(GenerateId(), Constants.MarketingType, "Campaign", marketingMsg);
+                        break;
+
+                    case "6":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Exiting Smart Notifications Hub. Goodbye!");
+        }
+
+        static int currentId = 1;
+        static int GenerateId() => currentId++;
+    }
+}
