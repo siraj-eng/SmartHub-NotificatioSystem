09d90e6de0324b485d9661fe3c2cab58583ff748
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-16 14:58:25 +0300

    SmartHub Notification System - Logger.cs

--- Utilities/Logger.cs
+++ Utilities/Logger.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace SmartHub_NotificatioSystem.Utilities
 {
-    internal class Logger
+    public static class Logger
     {
+        public static void LogInfo(string message) => Console.WriteLine($"[INFO] {message}");
+        public static void LogWarning(string message) => Console.WriteLine($"[WARNING] {message}");
+        public static void LogError(string message) => Console.WriteLine($"[ERROR] {message}");
     }
 }
